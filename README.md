f772fed8030c805837f20b808f3340186dcb325e
seanfarr788 <seanoliverfarrell@gmail.com>
2016-03-28 00:10:45 +0100

    Updated review.html

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #PCTOGO 
+last updated: 22:07 26/03/2016
+**HIA JACKIE!!**
 
-This is a website. 
+This is a website. Websites are fun. Fun is websites. is webistes fun? **nods**
--- scripts.js
+++ scripts.js
@@ -1,36 +1,9 @@
 
 function init() {
-	box = document.getElementById("box2Div");
 
-	box.style.animation = "	";
 }
 
-$(window).scroll(function() {
-    if (checkVisible($('#box2Div'))) {
-        scrollBoxes();
-    }
-});
 
 function autoScroll(amount) {
 	$('body,html').animate({ scrollTop: amount }, 800);
-}
-
-function scrollBoxes() {
-	box = document.getElementById("box2Div");
-
-	box.style.animation = "animateBox2 1s forwards"
-	box.style.animationdelay = "0.5s"
-}
-
-
-function checkVisible( element, evalType ) {
-    evalType = evalType || "visible";
-
-    var vpH = $(window).height(), 
-        st = $(window).scrollTop(), 
-        y = $(element).offset().top,
-        elementHeight = $(element).height();
-
-    if (evalType === "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
-    if (evalType === "above") return ((y < (vpH + st)));
 }
